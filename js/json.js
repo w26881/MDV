9295065ee6ea4e8b97385a6d2aafa133976c7a4e
Kevin Tresemer <kevintresemer@Kevins-MacBook-Pro.local>
2013-04-10 16:06:54 -0700

    MIU Setup

--- js/json.js
+++ js/json.js
@@ -18,7 +18,7 @@ var json = {
 		"name": ["Name:", "Danielle"],
 		"attraction": ["Attraction Level:", "10"],
 		"single": ["Single:", "Yes"],
-		"birthdate": ["Birthdate:", "08-20-1992"],
+		"birthdate": ["Birthdate:", "06-20-1992"],
 		"notes": ["Notes:", "Peonies"],
 		
 
@@ -28,178 +28,178 @@ var json = {
 		"name": ["Name:", "Natalie"],
 		"attraction": ["Attraction Level:", "7"],
 		"single": ["Single:", "Yes"],
-		"birthdate": ["Birthdate:", "03-13-1987"],
+		"birthdate": ["Birthdate:", "10-13-1987"],
 		"notes": ["Notes:", "Red Wine"],
 		
 
 	},
 	"relationship4": {
 		"status": ["Status:", "Prospect"],
-		"name": ["Name:", "Danielle"],
-		"attraction": ["Attraction Level:", "10"],
+		"name": ["Name:", "Rebecca"],
+		"attraction": ["Attraction Level:", "6"],
 		"single": ["Single:", "Yes"],
-		"birthdate": ["Birthdate:", "08-20-1992"],
-		"notes": ["Notes:", "Peonies"],
+		"birthdate": ["Birthdate:", "11-22-1990"],
+		"notes": ["Notes:", "Hiking"],
 		
 
 	},
 	"relationship5": {
 		"status": ["Status:", "Prospect"],
-		"name": ["Name:", "Danielle"],
+		"name": ["Name:", "Sarah"],
 		"attraction": ["Attraction Level:", "10"],
 		"single": ["Single:", "Yes"],
-		"birthdate": ["Birthdate:", "08-20-1992"],
-		"notes": ["Notes:", "Peonies"],
+		"birthdate": ["Birthdate:", "12-14-1987"],
+		"notes": ["Notes:", "Movies"],
 		
 
 	},
 	"relationship6": {
 		"status": ["Status:", "Prospect"],
-		"name": ["Name:", "Danielle"],
-		"attraction": ["Attraction Level:", "10"],
+		"name": ["Name:", "Denise"],
+		"attraction": ["Attraction Level:", "9"],
 		"single": ["Single:", "Yes"],
-		"birthdate": ["Birthdate:", "08-20-1992"],
-		"notes": ["Notes:", "Peonies"],
+		"birthdate": ["Birthdate:", "01-11-1991"],
+		"notes": ["Notes:", "Concerts"],
 		
 
 	},
 	"relationship7": {
 		"status": ["Status:", "Prospect"],
-		"name": ["Name:", "Danielle"],
-		"attraction": ["Attraction Level:", "10"],
+		"name": ["Name:", "Rachel"],
+		"attraction": ["Attraction Level:", "9"],
 		"single": ["Single:", "Yes"],
-		"birthdate": ["Birthdate:", "08-20-1992"],
-		"notes": ["Notes:", "Peonies"],
+		"birthdate": ["Birthdate:", "03-15-1989"],
+		"notes": ["Notes:", "Jewelry"],
 		
 
 	},
 	"relationship8": {
 		"status": ["Status:", "Prospect"],
-		"name": ["Name:", "Danielle"],
-		"attraction": ["Attraction Level:", "10"],
+		"name": ["Name:", "Amanda"],
+		"attraction": ["Attraction Level:", "8"],
 		"single": ["Single:", "Yes"],
-		"birthdate": ["Birthdate:", "08-20-1992"],
-		"notes": ["Notes:", "Peonies"],
+		"birthdate": ["Birthdate:", "04-19-1980"],
+		"notes": ["Notes:", "The Beach"],
 		
 
 	},
 	"relationship9": {
-		"status": ["Status:", "Prospect"],
-		"name": ["Name:", "Danielle"],
+		"status": ["Status:", "Ex"],
+		"name": ["Name:", "Jessica"],
 		"attraction": ["Attraction Level:", "10"],
 		"single": ["Single:", "Yes"],
-		"birthdate": ["Birthdate:", "08-20-1992"],
-		"notes": ["Notes:", "Peonies"],
+		"birthdate": ["Birthdate:", "07-12-1984"],
+		"notes": ["Notes:", "Travel"],
 		
 
 	},
 	"relationship10": {
 		"status": ["Status:", "Prospect"],
-		"name": ["Name:", "Danielle"],
-		"attraction": ["Attraction Level:", "10"],
+		"name": ["Name:", "Jennifer"],
+		"attraction": ["Attraction Level:", "7"],
 		"single": ["Single:", "Yes"],
-		"birthdate": ["Birthdate:", "08-20-1992"],
-		"notes": ["Notes:", "Peonies"],
+		"birthdate": ["Birthdate:", "06-20-1988"],
+		"notes": ["Notes:", "Dogs"],
 		
 
 	},
 	"relationship11": {
 		"status": ["Status:", "Prospect"],
-		"name": ["Name:", "Danielle"],
-		"attraction": ["Attraction Level:", "10"],
-		"single": ["Single:", "Yes"],
-		"birthdate": ["Birthdate:", "08-20-1992"],
-		"notes": ["Notes:", "Peonies"],
+		"name": ["Name:", "Kristen"],
+		"attraction": ["Attraction Level:", "9"],
+		"single": ["Single:", "No"],
+		"birthdate": ["Birthdate:", "08-28-1987"],
+		"notes": ["Notes:", "Cats"],
 		
 
 	},
 	"relationship12": {
-		"status": ["Status:", "Prospect"],
-		"name": ["Name:", "Danielle"],
+		"status": ["Status:", "Ex"],
+		"name": ["Name:", "Julie"],
 		"attraction": ["Attraction Level:", "10"],
 		"single": ["Single:", "Yes"],
-		"birthdate": ["Birthdate:", "08-20-1992"],
-		"notes": ["Notes:", "Peonies"],
+		"birthdate": ["Birthdate:", "02-04-1983"],
+		"notes": ["Notes:", "Hydrangeas"],
 		
 
 	},
 	"relationship13": {
 		"status": ["Status:", "Prospect"],
-		"name": ["Name:", "Danielle"],
-		"attraction": ["Attraction Level:", "10"],
+		"name": ["Name:", "Molly"],
+		"attraction": ["Attraction Level:", "8"],
 		"single": ["Single:", "Yes"],
-		"birthdate": ["Birthdate:", "08-20-1992"],
-		"notes": ["Notes:", "Peonies"],
+		"birthdate": ["Birthdate:", "03-07-1981"],
+		"notes": ["Notes:", "Shopping"],
 		
 
 	},
 	"relationship14": {
 		"status": ["Status:", "Prospect"],
-		"name": ["Name:", "Danielle"],
-		"attraction": ["Attraction Level:", "10"],
+		"name": ["Name:", "Stephanie"],
+		"attraction": ["Attraction Level:", "8"],
 		"single": ["Single:", "Yes"],
-		"birthdate": ["Birthdate:", "08-20-1992"],
-		"notes": ["Notes:", "Peonies"],
+		"birthdate": ["Birthdate:", "09-16-1982"],
+		"notes": ["Notes:", "Working Out"],
 		
 
 	},
 	"relationship15": {
 		"status": ["Status:", "Prospect"],
-		"name": ["Name:", "Danielle"],
-		"attraction": ["Attraction Level:", "10"],
+		"name": ["Name:", "McKenzie"],
+		"attraction": ["Attraction Level:", "7"],
 		"single": ["Single:", "Yes"],
-		"birthdate": ["Birthdate:", "08-20-1992"],
-		"notes": ["Notes:", "Peonies"],
+		"birthdate": ["Birthdate:", "07-15-1982"],
+		"notes": ["Notes:", "Politics"],
 		
 
 	},
 	"relationship16": {
 		"status": ["Status:", "Prospect"],
-		"name": ["Name:", "Danielle"],
-		"attraction": ["Attraction Level:", "10"],
-		"single": ["Single:", "Yes"],
-		"birthdate": ["Birthdate:", "08-20-1992"],
-		"notes": ["Notes:", "Peonies"],
+		"name": ["Name:", "Melissa"],
+		"attraction": ["Attraction Level:", "9"],
+		"single": ["Single:", "No"],
+		"birthdate": ["Birthdate:", "01-02-1979"],
+		"notes": ["Notes:", "Roses"],
 		
 
 	},
 	"relationship17": {
 		"status": ["Status:", "Prospect"],
-		"name": ["Name:", "Danielle"],
-		"attraction": ["Attraction Level:", "10"],
+		"name": ["Name:", "Ariana"],
+		"attraction": ["Attraction Level:", "9"],
 		"single": ["Single:", "Yes"],
-		"birthdate": ["Birthdate:", "08-20-1992"],
-		"notes": ["Notes:", "Peonies"],
+		"birthdate": ["Birthdate:", "03-09-1989"],
+		"notes": ["Notes:", "Breakfast in Bed"],
 		
 
 	},
 	"relationship18": {
 		"status": ["Status:", "Prospect"],
-		"name": ["Name:", "Danielle"],
+		"name": ["Name:", "Shannon"],
 		"attraction": ["Attraction Level:", "10"],
 		"single": ["Single:", "Yes"],
-		"birthdate": ["Birthdate:", "08-20-1992"],
-		"notes": ["Notes:", "Peonies"],
+		"birthdate": ["Birthdate:", "09-06-1988"],
+		"notes": ["Notes:", "Dancing"],
 		
 
 	},
 	"relationship19": {
 		"status": ["Status:", "Prospect"],
-		"name": ["Name:", "Danielle"],
+		"name": ["Name:", "Ciera"],
 		"attraction": ["Attraction Level:", "10"],
 		"single": ["Single:", "Yes"],
-		"birthdate": ["Birthdate:", "08-20-1992"],
-		"notes": ["Notes:", "Peonies"],
+		"birthdate": ["Birthdate:", "10-22-1985"],
+		"notes": ["Notes:", "Dinner"],
 		
 
 	},
 	"relationship20": {
 		"status": ["Status:", "Prospect"],
-		"name": ["Name:", "Danielle"],
-		"attraction": ["Attraction Level:", "10"],
-		"single": ["Single:", "Yes"],
-		"birthdate": ["Birthdate:", "08-20-1992"],
-		"notes": ["Notes:", "Peonies"],
+		"name": ["Name:", "Veronica"],
+		"attraction": ["Attraction Level:", "8"],
+		"single": ["Single:", "No"],
+		"birthdate": ["Birthdate:", "12-25-1983"],
+		"notes": ["Notes:", "Drinks"],
 		
 
 	}
