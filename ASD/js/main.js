a40d1c621ba19982d15566783a8b3b919e46a958
Kevin Tresemer <kevintresemer@Kevins-MacBook-Pro.local>
2013-06-10 19:18:41 -0700

    Week 2 first commit

--- ASD/js/main.js
+++ ASD/js/main.js
@@ -6,7 +6,22 @@ $('#home').on('pageinit', function(){
 });
 
 $('#add').on('pageinit', function(){
-	
+
+	function saveData(key) {
+		if(!key){
+			var id = Math.floor(Math.random()*1000001);
+		}else{
+			id = key;
+		}
+		var item 				= {};
+			item.status      = ["Status:", $("#status").val()];
+			item.name 		= ["Name: ", $("#name").val()];
+			item.tel				= ["Tel: ", $("#tel").val()];
+			item.notes		= ["Notes: ", $("#notes").val()];
+
+			localStorage.setItem(id, JSON.stringify(item));
+			alert("Relationship Saved!");
+	}
 });
 
 $('#display').on('pageinit', function(){
