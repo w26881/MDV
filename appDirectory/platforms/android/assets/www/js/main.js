7658f8ad510a7f8d619a37136fd07f42b0bfb9d4
Kevin Tresemer <kevintresemer@Kevins-MacBook-Pro.local>
2013-10-10 14:49:28 -0700

    Week 2

--- appDirectory/platforms/android/assets/www/js/main.js
+++ appDirectory/platforms/android/assets/www/js/main.js
@@ -1,3 +1,50 @@
 /*Kevin Tresemer
 	  AVF 1310*/
+
+document.addEventListener("deviceready", deviceReady, false);
+
+function deviceReady(){
+
+}
 	  
+$('#getInstagram').on('click', function() {
+  var url = "https://api.instagram.com/v1/media/popular?callback=?&amp;client_id=6204a71b952a4411a5a448039fe4b23d&amp";
+  $.getJSON(url, instaOutput);
+});
+
+var instaOutput = function(info) {
+    
+    alert("instaOutput");
+    console.log(info);
+    
+    $.each(info.data, function(index, photo) {
+           var picture = "<li align=center><img src='" + photo.images.thumbnail.url + "' alt='" + photo.user.full_name + "' /></li>";
+           $("#instagramOutput").append(picture);
+           });
+};
+
+
+$('#getWeather').on('click', function() {
+    $.ajax({
+           url : "http://api.wunderground.com/api/a345a5ccd8789cad/conditions/q/OR/Medford.json",
+           dataType : "jsonp",
+           success : function(weatherInfo) {
+           console.log(weatherInfo);
+           
+           var weatherImage = weatherInfo.current_observation.image.url;
+           var city = weatherInfo.current_observation.display_location.city;
+           var state = weatherInfo.current_observation.display_location.state;
+           var temp = weatherInfo.current_observation.temp_f;
+
+           
+           $("#weatherOutput")
+           .append("<li>" + "<p>" + "<img src='" +
+                   weatherImage + "'/><br/>" + "<h2>" +
+                   city + ", " + state + "</h2>" +
+                   "Current Temp: " + temp + "°F" + "<br/>" +
+                   "</p>" + "</li>");
+           
+           }
+           });
+    
+                    })
